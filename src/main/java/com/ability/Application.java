05a94aabd7350704296c12903db5d598714f9a8a
dapengchina <aoqishequ@163.com>
2018-06-26 18:29:34 +0800

    Change-Id: I333ef33a6415daad8742c183802f68766d56ad65

--- src/main/java/com/ability/Application.java
+++ src/main/java/com/ability/Application.java
@@ -7,17 +7,16 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-// 开启通用注解扫描
-@ComponentScan
-public class Application extends SpringBootServletInitializer {
+public class Application{
 	/**
 	 * 实现SpringBootServletInitializer可以让spring-boot项目在web容器中运行
 	 */
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		builder.sources(this.getClass());
-		return super.configure(builder);
-	}
+//	@Override extends SpringBootServletInitializer // 开启通用注解扫描
+//	@ComponentScan
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		builder.sources(this.getClass());
+//		return super.configure(builder);
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
