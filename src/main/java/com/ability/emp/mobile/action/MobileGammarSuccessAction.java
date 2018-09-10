cc261706961a24cae92b1bbedb040133345814de
dapengchina <aoqishequ@163.com>
2018-09-10 09:49:53 +0800

    Change-Id: I939022415c8093b36037bd2966e0c14727edd2da

--- src/main/java/com/ability/emp/mobile/action/MobileGammarSuccessAction.java
+++ src/main/java/com/ability/emp/mobile/action/MobileGammarSuccessAction.java
@@ -34,9 +34,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @CrossOrigin//解决跨域请求
 @Controller
-@RequestMapping("/mobile/getFillBlankData")
-public class MobileFillBlankDropLetAction {
-	
+@RequestMapping("/mobile/gammarSuccess")
+public class MobileGammarSuccessAction {
 	
 	
 	@Resource
@@ -61,91 +60,17 @@ public class MobileFillBlankDropLetAction {
 	
 	
 	
-	@RequestMapping("/getFillBlankData/{dropLetId}/{dropLetConfTypeId}")
+	@RequestMapping("/gammarSuccess/{dropLetId}/{dropLetConfTypeId}")
 	@ResponseBody
-	public String getFillBlankData(@PathVariable("dropLetId") String dropLetId,@PathVariable("dropLetConfTypeId") String dropLetConfTypeId) throws Exception {
-		List<DropLetButtonVo> list = new ArrayList<DropLetButtonVo>();
-		Map<String,Object> map = new HashMap<String,Object>();
-		MobileFillBlankDropLetEntity me = new MobileFillBlankDropLetEntity();
-		MobileDropLetEntity md = new MobileDropLetEntity();
-		if(!dropLetId.equals("1")){
-			me.setDropletid(dropLetId);//确认是哪个droplet下的数据
-		}
-		me.setDropletconftypeid(dropLetConfTypeId);;//确认是哪个位置的数据
+	public String gammarSuccess(@PathVariable("dropLetId") String dropLetId,@PathVariable("dropLetConfTypeId") String dropLetConfTypeId) throws Exception {
 		
-		MobileFillBlankDropLetEntity fillBlankEntity = mobileFillBlankDropLetService.getFillBlankData(me);
-		
-		FillBlankDropLetVo sv = new FillBlankDropLetVo();
-		sv.setDropletid(fillBlankEntity.getDropletid());
-		sv.setDropletconftypeid(fillBlankEntity.getDropletconftypeid());
-		sv.setReladropletid(fillBlankEntity.getReladropletid());
-		sv.setReladropletconftypeid(fillBlankEntity.getReladropletconftypeid());
-		sv.setImage(SysConstant.SERVICE_HOST+fillBlankEntity.getImage());
-		
-		md.setId(fillBlankEntity.getReladropletid());
-		MobileDropLetEntity mo = mobileDropLetService.getDropLetByID(md);
-		if(mo!=null){
-			sv.setDropLetLink(mo.getDropletlink());
-		}
-		/**
-		 * 获取问题
-		 */
-		MobileFillBlankQuestionEntity mq = new MobileFillBlankQuestionEntity();
-		mq.setFillblankdataid(fillBlankEntity.getId());
-		List<MobileFillBlankQuestionEntity> questList = mobileFillBlankQuestionService.getFillBlankQuestion(mq);
-		//int[] emptyposition;
-		String empty = "";
-		List<FillBlankQuestionBean> questBeanList = new ArrayList<FillBlankQuestionBean>();
-		for(int i=0;i<questList.size();i++){
-			FillBlankQuestionBean questBean = new FillBlankQuestionBean();
-			questBean.setTit(questList.get(i).getTit());
-			questBean.setCorrect(questList.get(i).getCorrect());
-			questBean.setNum(questList.get(i).getNum());
-			questBean.setIfem(questList.get(i).getIfem().equals("false")?false:true);
-			
-			//组装空位数组
-			if(questList.get(i).getIfem().equals("true")){
-				empty=empty+i+",";
-			}
-			
-			questBeanList.add(questBean);
-		}
-		String[] emptystring = empty.split(",");
-		int[] emptyposition = new int[emptystring.length];
-		for(int n=0;n<emptystring.length;n++){
-			emptyposition[n] = Integer.parseInt(emptystring[n]);
-		}
-		sv.setQuest(questBeanList);
-		sv.setEmptyposition(emptyposition);
-		
-		/**
-		 * 获取答案
-		 */
-		MobileFillBlankAnswerEntity ans = new MobileFillBlankAnswerEntity();
-		ans.setFillblankdataid(fillBlankEntity.getId());
-		List<MobileFillBlankAnswerEntity> fillBlankAnswerList = mobileFillBlankAnswerService.getFillBlankAnswer(ans);
-		List<FillBlankAnswerBean> answerBeanlist = new ArrayList<FillBlankAnswerBean>();
-		for(int j=0;j<fillBlankAnswerList.size();j++){
-			FillBlankAnswerBean fb = new FillBlankAnswerBean();
-			fb.setTit(fillBlankAnswerList.get(j).getTit());
-			fb.setSubnum(fillBlankAnswerList.get(j).getSubnum());
-			
-			answerBeanlist.add(fb);
-		}
-		sv.setAnswer(answerBeanlist);
-		
-		
-			
-		/**
-		 * 获取button
-		 */
-		if(fillBlankEntity!=null 
-		   && fillBlankEntity.getDropletconftypeid()!=null
-		   && !"".equals(fillBlankEntity.getDropletconftypeid())
-		){
+		   List<DropLetButtonVo> list = new ArrayList<DropLetButtonVo>();
+		   /**
+		    * 获取button
+		    */
 			MobileDropLetConfTypeEntity mde = new MobileDropLetConfTypeEntity();
-			mde.setDropletconftype(fillBlankEntity.getDropletconftypeid());//确定是哪个位置
-			mde.setDropletid(fillBlankEntity.getDropletid());//确定是哪个droplet
+			mde.setDropletconftype(dropLetConfTypeId);//确定是哪个位置
+			mde.setDropletid(dropLetId);//确定是哪个droplet
 			
 			MobileDropLetConfTypeEntity remde = mobileDropLetConfTypeService.getDropLetConfigType(mde);
 			if(remde!=null){
@@ -204,12 +129,9 @@ public class MobileFillBlankDropLetAction {
 					}
                 }
 			}
-		}
 		
 		//组合数据
-		map.put("fillBlank", sv);
-		map.put("button", list);
-		return objectMapper.writeValueAsString(map);
+		return objectMapper.writeValueAsString(list);
 	}
 
 }
