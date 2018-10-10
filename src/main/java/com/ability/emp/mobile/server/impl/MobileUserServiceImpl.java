1c130523eccc9305a1419fb04d972fa533612ac2
dapengchina <aoqishequ@163.com>
2018-10-10 11:46:44 +0800

    Change-Id: Id2494777ad4360a556e0c39833d50dcb7d342529

--- src/main/java/com/ability/emp/mobile/server/impl/MobileUserServiceImpl.java
+++ src/main/java/com/ability/emp/mobile/server/impl/MobileUserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.ability.emp.mobile.server.impl;
 
+
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -25,21 +28,12 @@ import com.ability.emp.mobile.dao.MobileUserDao;
 import com.ability.emp.mobile.dao.MobileUserTaskDao;
 import com.ability.emp.mobile.dao.MobileVideoDropLetDao;
 import com.ability.emp.mobile.entity.MobileCardListDropletEntity;
-import com.ability.emp.mobile.entity.MobileChoiceAudioDropletEntity;
-import com.ability.emp.mobile.entity.MobileChoiceTextDropLetEntity;
-import com.ability.emp.mobile.entity.MobileDictationDropLetEntity;
-import com.ability.emp.mobile.entity.MobileFillBlankDropLetEntity;
-import com.ability.emp.mobile.entity.MobileMatchDropLetEntity;
-import com.ability.emp.mobile.entity.MobileReadSpeakDropLetEntity;
 import com.ability.emp.mobile.entity.MobileSceCategoryEntity;
 import com.ability.emp.mobile.entity.MobileScenListDropLetEntity;
-import com.ability.emp.mobile.entity.MobileScenarioDropLetEntity;
-import com.ability.emp.mobile.entity.MobileSortDropLetEntity;
 import com.ability.emp.mobile.entity.MobileSubTaskEntity;
 import com.ability.emp.mobile.entity.MobileTaskEntity;
 import com.ability.emp.mobile.entity.MobileUserEntity;
 import com.ability.emp.mobile.entity.MobileUserTaskEntity;
-import com.ability.emp.mobile.entity.MobileVideoDropLetEntity;
 import com.ability.emp.mobile.server.MobileUserService;
 import com.ability.emp.util.UUIDUtil;
 
@@ -136,6 +130,9 @@ public class MobileUserServiceImpl implements MobileUserService{
 		MobileUserTaskEntity userTask = new MobileUserTaskEntity();//用户任务
 		MobileSubTaskEntity subtask = new MobileSubTaskEntity();//子任务
 		MobileTaskEntity task = new MobileTaskEntity();//任务
+		Calendar calender = Calendar.getInstance();
+        calender.setTime(new Date());
+        calender.add(Calendar.MONTH, 2);
 		
 		int i = mobileUserDao.insert(mue);
 		if(i>0){
@@ -155,7 +152,9 @@ public class MobileUserServiceImpl implements MobileUserService{
 					task.setCourseid(courseid[k]);
 					task.setTaskname(recourse.getScecatname());
 					task.setTaskstate(SysConstant.TASK_STATE0);//未结束
-					//task.setThesaures_Type(thesaures_Type);
+					task.setStartDate(new Date());//任务开始日期
+					task.setEndDate(calender.getTime());//任务结束日期,自动默认2个月
+					task.setThesauresType("-1");
 					mobileTaskDao.insert(task);
 					//保存用户任务
 					userTask.setId(UUIDUtil.generateUUID());
@@ -189,15 +188,6 @@ public class MobileUserServiceImpl implements MobileUserService{
 		MobileSubTaskEntity subtask = new MobileSubTaskEntity();//子任务
 		MobileScenListDropLetEntity scenListDropLet = new MobileScenListDropLetEntity();
 		MobileCardListDropletEntity cardListDropLet = new MobileCardListDropletEntity();
-		MobileScenarioDropLetEntity scenarioDropLet = new MobileScenarioDropLetEntity();
-		MobileChoiceTextDropLetEntity choiceTextDropLet = new MobileChoiceTextDropLetEntity();
-		MobileFillBlankDropLetEntity fillBlankDropLet = new MobileFillBlankDropLetEntity();
-		MobileChoiceAudioDropletEntity choiceAudioDropLet = new MobileChoiceAudioDropletEntity();
-		MobileReadSpeakDropLetEntity readSpeakDropLet = new MobileReadSpeakDropLetEntity();
-		MobileSortDropLetEntity sortDropLet = new MobileSortDropLetEntity();
-		MobileMatchDropLetEntity matchDropLet = new MobileMatchDropLetEntity();
-		MobileVideoDropLetEntity videoDropLet = new MobileVideoDropLetEntity();
-		MobileDictationDropLetEntity dictationDropLet = new MobileDictationDropLetEntity();
 		
 		//根据课程ID判断需要查询哪个表,用以保存到子任务
 		if(courseid.equals(SysConstant.DROPLET_ID2)){
@@ -242,152 +232,6 @@ public class MobileUserServiceImpl implements MobileUserService{
 					handleSubTask(cardList.get(j).getReladropletid(),cardList.get(j).getReladropletid(),cardList.get(j).getReladropletcontypeid(),taskid);
 				}
 			}
-		}
-		if(courseid.equals(SysConstant.DROPLET_ID4)){
-			//ScenarioDropLet
-			scenarioDropLet.setDropletid(dropLetID);
-			scenarioDropLet.setDropletconftypeid(dropLetConfTypeID);
-			List<MobileScenarioDropLetEntity> scenario = mobileScenarioDropLetDao.selectScenarioData(scenarioDropLet);
-			
-			for(int i=0;i<scenario.size();i++){
-				//保存subtask
-				subtask.setId(UUIDUtil.generateUUID());
-				subtask.setDropletid(scenario.get(i).getDropletid());
-				subtask.setDropletconftypeid(scenario.get(i).getDropletconftypeid());
-				subtask.setTaskid(taskid);
-				mobileSubTaskDao.insert(subtask);
-			}
-			for(int j=0;j<scenario.size();j++){
-				if(scenario.get(j).getReladropletid()!=null && !"".equals(scenario.get(j).getReladropletid())){
-					handleSubTask(scenario.get(j).getReladropletid(),scenario.get(j).getReladropletid(),scenario.get(j).getReladropletconftypeid(),taskid);
-				}
-			}
-		}
-		if(courseid.equals(SysConstant.DROPLET_ID5)){
-			//TextChoiceDropLet
-			choiceTextDropLet.setDropletid(dropLetID);
-			choiceTextDropLet.setDropletconftypeid(dropLetConfTypeID);
-			MobileChoiceTextDropLetEntity choiceText = mobileChoiceTextDropLetDao.selectChoiceDropLetData(choiceTextDropLet);
-			
-			//保存subtask
-			subtask.setId(UUIDUtil.generateUUID());
-			subtask.setDropletid(choiceText.getDropletid());
-			subtask.setDropletconftypeid(choiceText.getDropletconftypeid());
-			subtask.setTaskid(taskid);
-			mobileSubTaskDao.insert(subtask);
-			handleSubTask(choiceText.getReladropletid(),choiceText.getReladropletid(),choiceText.getReladropletconftypeid(),taskid);
-		}
-		if(courseid.equals(SysConstant.DROPLET_ID6)){
-			//FillBlankDropLet
-			fillBlankDropLet.setDropletid(dropLetID);
-			fillBlankDropLet.setDropletconftypeid(dropLetConfTypeID);
-			MobileFillBlankDropLetEntity fillBlank = mobileFillBlankDropLetDao.selectFillBlankData(fillBlankDropLet);
-			
-			//保存subtask
-			subtask.setId(UUIDUtil.generateUUID());
-			subtask.setDropletid(fillBlank.getDropletid());
-			subtask.setDropletconftypeid(fillBlank.getDropletconftypeid());
-			subtask.setTaskid(taskid);
-			mobileSubTaskDao.insert(subtask);
-			handleSubTask(fillBlank.getReladropletid(),fillBlank.getReladropletid(),fillBlank.getReladropletconftypeid(),taskid);
-		}
-		if(courseid.equals(SysConstant.DROPLET_ID7)){
-			//SuccessDropLet
-		}
-		if(courseid.equals(SysConstant.DROPLET_ID8)){
-			//AudioChoiceDropLet
-			choiceAudioDropLet.setDropletid(dropLetID);
-			choiceAudioDropLet.setDropletconftypeid(dropLetConfTypeID);
-			MobileChoiceAudioDropletEntity choiceAudio = mobileChoiceAudioDropletDao.selectChoiceAudioDropletData(choiceAudioDropLet);
-			
-			//保存subtask
-			subtask.setId(UUIDUtil.generateUUID());
-			subtask.setDropletid(choiceAudio.getDropletid());
-			subtask.setDropletconftypeid(choiceAudio.getDropletconftypeid());
-			subtask.setTaskid(taskid);
-			mobileSubTaskDao.insert(subtask);
-			
-			handleSubTask(choiceAudio.getReladropletid(),choiceAudio.getReladropletid(),choiceAudio.getReladropletconftypeid(),taskid);
-		}
-		if(courseid.equals(SysConstant.DROPLET_ID9)){
-			//ReadSpeakDropLet
-			readSpeakDropLet.setDropletid(dropLetID);
-			readSpeakDropLet.setDropletconftypeid(dropLetConfTypeID);
-			MobileReadSpeakDropLetEntity readSpeak = mobileReadSpeakDropLetDao.selectReadSpeakData(readSpeakDropLet);
-			
-			//保存subtask
-			subtask.setId(UUIDUtil.generateUUID());
-			subtask.setDropletid(readSpeak.getDropletid());
-			subtask.setDropletconftypeid(readSpeak.getDropletconftypeid());
-			subtask.setTaskid(taskid);
-			mobileSubTaskDao.insert(subtask);
-			
-			handleSubTask(readSpeak.getReladropletid(),readSpeak.getReladropletid(),readSpeak.getReladropletconftypeid(),taskid);
-		}
-		if(courseid.equals(SysConstant.DROPLET_ID10)){
-			//SortDropLet
-			sortDropLet.setDropletid(dropLetID);
-			sortDropLet.setDropletconftypeid(dropLetConfTypeID);
-			List<MobileSortDropLetEntity> sortDropList = mobileSortDropLetDao.selectSortData(sortDropLet);
-			
-			for(int i=0;i<sortDropList.size();i++){
-				//保存subtask
-				subtask.setId(UUIDUtil.generateUUID());
-				subtask.setDropletid(sortDropList.get(i).getDropletid());
-				subtask.setDropletconftypeid(sortDropList.get(i).getDropletconftypeid());
-				subtask.setTaskid(taskid);
-				mobileSubTaskDao.insert(subtask);
-			}
-			for(int j=0;j<sortDropList.size();j++){
-				if(sortDropList.get(j).getReladropletid()!=null && !"".equals(sortDropList.get(j).getReladropletid())){
-					handleSubTask(sortDropList.get(j).getReladropletid(),sortDropList.get(j).getReladropletid(),sortDropList.get(j).getReladropletconftypeid(),taskid);
-				}
-			}
-		}
-		if(courseid.equals(SysConstant.DROPLET_ID11)){
-			//MatchDropLet
-			matchDropLet.setDropletid(dropLetID);
-			matchDropLet.setDropletconftypeid(dropLetConfTypeID);
-			MobileMatchDropLetEntity match = mobileMatchDropLetDao.selectMatchData(matchDropLet);
-			
-			//保存subtask
-			subtask.setId(UUIDUtil.generateUUID());
-			subtask.setDropletid(match.getDropletid());
-			subtask.setDropletconftypeid(match.getDropletconftypeid());
-			subtask.setTaskid(taskid);
-			mobileSubTaskDao.insert(subtask);
-			
-			handleSubTask(match.getReladropletid(),match.getReladropletid(),match.getReladropletconftypeid(),taskid);
-		}
-		if(courseid.equals(SysConstant.DROPLET_ID12)){
-			//VideoDropLet
-			videoDropLet.setDropletid(dropLetID);
-			videoDropLet.setDropletconftypeid(dropLetConfTypeID);
-			MobileVideoDropLetEntity video = mobileVideoDropLetDao.selectVideoData(videoDropLet);
-			
-			//保存subtask
-			subtask.setId(UUIDUtil.generateUUID());
-			subtask.setDropletid(video.getDropletid());
-			subtask.setDropletconftypeid(video.getDropletconftypeid());
-			subtask.setTaskid(taskid);
-			mobileSubTaskDao.insert(subtask);
-			
-			handleSubTask(video.getReladropletid(),video.getReladropletid(),video.getDropletconftypeid(),taskid);
-		}
-		if(courseid.equals(SysConstant.DROPLET_ID13)){
-			//DictationDropLet
-			dictationDropLet.setDropletid(dropLetID);
-			dictationDropLet.setDropletconftypeid(dropLetConfTypeID);
-			MobileDictationDropLetEntity dictation = mobileDictationDropLetDao.selectDictationData(dictationDropLet);
-			
-			//保存subtask
-			subtask.setId(UUIDUtil.generateUUID());
-			subtask.setDropletid(dictation.getDropletid());
-			subtask.setDropletconftypeid(dictation.getDropletconftypeid());
-			subtask.setTaskid(taskid);
-			mobileSubTaskDao.insert(subtask);
-			
-			handleSubTask(dictation.getReladropletid(),dictation.getReladropletid(),dictation.getDropletconftypeid(),taskid);
 		}
 	}
 	
